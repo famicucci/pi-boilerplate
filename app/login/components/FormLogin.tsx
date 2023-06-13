f7d310bee1e4cffdb512cd933ac3f232e2b42398
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-06-13 09:51:22 +0200

    Reorder Login component

---
+++ app/login/components/FormLogin.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styles from '../styles/formLogin.module.css';
+
+export default function FormLogin() {
+	return (
+		<form className={styles.formInputs}>
+			<input type="text" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<button type="submit">Login</button>
+		</form>
+	);
+}
--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,6 +1,5 @@
-import styles from './layout.module.css';
+import styles from './styles/layout.module.css';
 
-// Este es el componente que se repite en todas las páginas, por lo tanto aqui debe ir el header y el footer
 function Home({ children }) {
 	return <main className={styles.main}>{children}</main>;
 }
