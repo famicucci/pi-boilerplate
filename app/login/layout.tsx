515e448ab522cd2a47e0492d39e863ab090fc87e
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-07-12 12:40:10 +0200

    Add model to Home

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,6 +1,7 @@
+import { Children } from '@/models/children';
 import styles from './styles/layout.module.css';
 
-function Home({ children }) {
+function Home({ children }: Children) {
 	return <main className={styles.main}>{children}</main>;
 }
 
