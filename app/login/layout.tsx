177354e309ebcc64a30ff74c2277146dedf6de18
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-07-12 16:56:05 +0200

    Add background to login

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -2,7 +2,11 @@ import { Children } from '@/models';
 import styles from './styles/layout.module.css';
 
 function Home({ children }: Children) {
-	return <main className={styles.main}>{children}</main>;
+	return (
+		<main className={`${styles.alignCenter} ${styles.background}`}>
+			{children}
+		</main>
+	);
 }
 
 export default Home;
