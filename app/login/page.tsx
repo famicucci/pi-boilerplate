f219a336ad95af3d4ca5c05b8cf1bbe53c819df7
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-07-12 12:33:41 +0200

    Remove div

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,11 @@
 import { Container } from './components/Container';
 import { FormLogin } from './components/FormLogin';
-import styles from './styles/formLogin.module.css';
 
 function Login() {
 	return (
-		<div className={styles.loginForm}>
-			<Container>
-				<FormLogin />
-			</Container>
-		</div>
+		<Container>
+			<FormLogin />
+		</Container>
 	);
 }
 
