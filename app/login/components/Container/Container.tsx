11f70d99106ec6f5153198d50fc5140dd16f2aa7
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-07-12 12:38:36 +0200

    Add children model

--- app/login/components/Container/Container.tsx
+++ app/login/components/Container/Container.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import styles from '../../styles/container.module.css';
+import { Children } from '@/models/children';
 
-export interface Container {
-	children: React.ReactNode;
-}
-
-export default function Container({ children }: Container) {
+export default function Container({ children }: Children) {
 	return <div className={styles.container}>{children}</div>;
 }
