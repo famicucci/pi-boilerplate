bff7a8e439351dd3bab38298eb599080893dc876
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-07-14 11:31:56 +0200

    Add Logo to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,11 @@
+import { Logo } from '@/components/Logo';
 import { Container } from './components/Container';
 import { FormLogin } from './components/FormLogin';
 
 function Login() {
 	return (
 		<Container>
+			<Logo />
 			<FormLogin />
 		</Container>
 	);
