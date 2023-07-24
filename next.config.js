52565203aa72d1db982af30a2fe47cac1af76e18
Francisco Micucci <fmicucci@piconsulting.com.ar>
2023-07-24 10:49:23 +0200

    Add headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'Content-Security-Policy',
+						value: "default-src 'self'",
+					},
+					{
+						key: 'X-Content-Type-Options',
+						value: 'nosniff',
+					},
+					{
+						key: 'X-Frame-Options',
+						value: 'DENY',
+					},
+					{
+						key: 'Referrer-Policy',
+						value: 'strict-origin-when-cross-origin',
+					},
+					{
+						key: 'Strict-Transport-Security',
 
-module.exports = nextConfig
+						value: 'max-age=31536000; includeSubDomains; preload',
+					},
+					{
+						key: 'Permissions-Policy',
+						value: 'geolocation=none, microphone=none, camera=none',
+					},
+				],
+			},
+		];
+	},
+};
+
+module.exports = nextConfig;
