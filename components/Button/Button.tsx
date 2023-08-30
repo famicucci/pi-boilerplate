1eb6b8adf02c3711fe2c729ef33dc2728b7045a6
Francisco Agustín Micucci <famicucci@gmail.com>
2023-08-30 15:01:26 +0200

    Refactor button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -3,12 +3,20 @@ import * as React from 'react';
 import styles from '../../styles/button.module.css';
 
 const Button = ({ color, type, children, className }: Button & Children) => {
+	const colors: {
+		primary: string;
+		secondary: string;
+		none: string;
+	} = {
+		primary: styles.colorPrimary,
+		secondary: styles.colorSecondary,
+		none: '',
+	};
+
 	return (
 		<button
 			type={type === 'submit' ? 'submit' : 'button'}
-			className={`${className} ${styles.button} ${
-				color === 'primary' ? styles.colorPrimary : ''
-			} ${color === 'secondary' ? styles.colorSecondary : ''}`}
+			className={`${className} ${styles.button} ${colors[color]}`}
 		>
 			{children}
 		</button>
