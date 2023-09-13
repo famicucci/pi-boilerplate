d6d5bf94ef6431ae079d97f96359e101c4e1603f
Francisco Agustín Micucci <famicucci@gmail.com>
2023-09-13 09:04:43 -0300

    Add transition to the button

--- app/(loggedIn)/home/components/Buttons/ButtonAddUser.tsx
+++ app/(loggedIn)/home/components/Buttons/ButtonAddUser.tsx
@@ -1,23 +1,27 @@
 'use client';
 import { Button } from '@/components/Button';
-import * as React from 'react';
+import React, { useTransition } from 'react';
 import styles from '../../styles/buttonAddUser.module.css';
 import { createUserRequest } from '../../services';
 
 export function ButtonAddUser() {
+	const [isPending, startTransition] = useTransition();
+
 	return (
 		<Button
 			color="primary"
 			className={styles.buttonAddUser}
 			onClick={() =>
-				createUserRequest({
-					name: 'Victoria',
-					age: 27,
-					city: 'Buenos Aires',
-				})
+				startTransition(() =>
+					createUserRequest({
+						name: 'Victoria',
+						age: 27,
+						city: 'Buenos Aires',
+					})
+				)
 			}
 		>
-			Agregar Usuario
+			{isPending ? 'Agregando el usuario...' : 'Agregar Usuario'}
 		</Button>
 	);
 }
